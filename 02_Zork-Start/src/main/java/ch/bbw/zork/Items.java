b0756ea10c6e0f5599337afb6d5dc18600739880
HarounAhmad <68501385+HarounAhmad@users.noreply.github.com>
2021-01-05 19:09:38 +0100

    Finished map

--- 02_Zork-Start/src/main/java/ch/bbw/zork/Items.java
+++ 02_Zork-Start/src/main/java/ch/bbw/zork/Items.java
@@ -1,16 +1,38 @@
 package ch.bbw.zork;
 
+/**
+ * The type Items.
+ */
 public class Items {
 
+    /**
+     * The Itemname.
+     */
     String Itemname = null;
+    /**
+     * The Weight kg.
+     */
     float WeightKG;
+    /**
+     * The Id.
+     */
     int ID;
 
+    /**
+     * Sets .
+     *
+     * @param itemname the itemname
+     */
     void setitemname(String itemname) {
 
         Itemname = itemname;
     }
 
+    /**
+     * Sets weight kg.
+     *
+     * @param weight the weight
+     */
     void setWeightKG(float weight) {
         WeightKG = weight;
     }
--- 02_Zork-Start/src/main/java/ch/bbw/zork/Zork2.java
+++ 02_Zork-Start/src/main/java/ch/bbw/zork/Zork2.java
@@ -1,7 +1,15 @@
 package ch.bbw.zork;
 
+/**
+ * The type Zork 2.
+ */
 public class Zork2 {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Game zorkgame = new Game();
         zorkgame.play();
